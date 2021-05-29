56938bbb63829b4826fed6f102d148a130c12bc5
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-05-29 11:46:42 +0300

    Initial Commit

---
+++ Audio Conversion Command Line/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Audio Conversion Command Line
+//
+//  Created by Panayotis Matsinopoulos on 29/5/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
